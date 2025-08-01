161b8ab997225ccd619feda261139a373a22ee30
Guuzta <Guuhsilva13@outlook.com>
2025-08-01 20:47:03 -0300

    adicionando validacao no componente do formulario

--- src/components/UserForm.js
+++ src/components/UserForm.js
@@ -1,20 +1,114 @@
-import { TextField, Typography, Box, Button, Grid } from '@mui/material'
+import { useState } from 'react'
+import { 
+    TextField, 
+    Typography, 
+    Box, 
+    Button, 
+    Grid 
+} from '@mui/material'
 
 const UserForm = () => {
+
+    const [form, setForm] = useState({
+        name: {
+            value: '',
+            error: false
+        },
+        job: {
+            value: '',
+            error: false
+        },
+        phone: {
+            value: '',
+            error: false
+        },
+        email: {
+            value: '',
+            error: false
+        }
+    })
+
+    const handleInputChange = (e) => {
+        const { name, value } = e.target
+
+        setForm({
+            ...form,
+            [name]: {
+                value
+            }
+        })
+
+        console.log(form)
+    }
+
+    const handleRegisterButton = () => {
+        let hasError = false
+
+        let newFormState = {
+            ...form,
+        }
+
+        if (!form.name.value) {
+            hasError = true
+
+            newFormState.name = {
+                value: form.name.value,
+                error: true,
+                helperText: 'Preencha o campo nome!'
+            }
+        }
+
+        if (!form.job.value) {
+            hasError = true
+
+            newFormState.job = {
+                value: form.job.value,
+                error: true,
+                helperText: 'Preencha o campo cargo!'
+            }
+        }
+
+        if (!form.phone.value) {
+            hasError = true
+
+            newFormState.phone = {
+                value: form.phone.value,
+                error: true,
+                helperText: 'Preencha o campo telefone!'
+            }
+        }
+
+        if (!form.email.value) {
+            hasError = true
+
+            newFormState.email = {
+                value: form.email.value,
+                error: true,
+                helperText: 'Preencha o campo email!'
+            }
+        }
+
+
+        if (hasError) {
+            setForm(newFormState)
+        }
+    }
+
     return (
-        <Box 
-            component='form' 
+
+        <Box
+            component='form'
             autoComplete='off'
             textAlign='center'
-          >
-
+            sx={{ '& .MuiFormHelperText-root': { marginLeft: 0.5, marginTop: 1 } }}
+        >
             <Grid
                 container
-                rowSpacing={4}
+                rowSpacing={3}
                 sx={{
                     flexDirection: 'column',
                     justifyContent: 'center',
-                    alignItems: 'center'   
+                    alignItems: 'center'
                 }}
             >
                 <Grid size={12}>
@@ -24,23 +118,65 @@ const UserForm = () => {
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <TextField name='name' label='Nome completo' variant='outlined' sx={{ width: '100%'}}/>
+                    <TextField
+                        error={form.name.error}
+                        helperText={form.name.error ? form.name.helperText : ''}
+                        onChange={handleInputChange}
+                        value={form.name.value}
+                        name='name'
+                        label='Nome completo'
+                        variant='outlined'
+                        sx={{ width: '100%' }}
+                    />
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <TextField name='job' label='Cargo' variant='outlined'  sx={{ width: '100%'}}/>
+                    <TextField
+                        error={form.job.error}
+                        helperText={form.job.error ? form.job.helperText : ''}
+                        onChange={handleInputChange}
+                        value={form.job.value}
+                        name='job'
+                        label='Cargo'
+                        variant='outlined'
+                        sx={{ width: '100%' }} />
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <TextField name='phone' label='Telefone' variant='outlined' sx={{ width: '100%'}}/>
+                    <TextField
+                        error={form.phone.error}
+                        helperText={form.phone.error ? form.phone.helperText : ''}
+                        onChange={handleInputChange}
+                        name='phone'
+                        label='Telefone'
+                        variant='outlined'
+                        sx={{ width: '100%' }}
+                    />
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <TextField name='email' label='Email' variant='outlined' sx={{ width: '100%'}}/>
+                    <TextField
+                        error={form.email.error}
+                        helperText={form.email.error ? form.email.helperText : ''}
+                        onChange={handleInputChange}
+                        name='email'
+                        label='Email'
+                        variant='outlined'
+                        sx={{ width: '100%' }}
+                    />
                 </Grid>
 
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <Button variant='contained' sx={{ width: '100%', padding: '10px 0px'}}>Gerar Cartão!</Button>
+                    <Button
+                        onClick={handleRegisterButton}
+                        variant='contained'
+                        sx={{
+                            width: '100%',
+                            padding: '10px 0px'
+                        }}
+                    >
+                        Gerar Cartão!
+                    </Button>
                 </Grid>
 
             </Grid>
