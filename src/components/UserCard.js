54780c7c1b48abdfff540fb4529b8019f9746139
Guuzta <Guuhsilva13@outlook.com>
2025-08-02 13:56:27 -0300

    modificando estrutura do componente do cartao para receber as props

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,53 +1,50 @@
-import { 
-    Grid, 
-    Container, 
-    Avatar, 
-    Card, 
-    CardContent, 
-    Typography 
+import {
+    Grid,
+    Container,
+    Avatar,
+    Card,
+    CardContent,
+    Typography
 } from '@mui/material'
 
-const card = (
-    <>
-        <CardContent 
-            sx={{ 
-                padding: '30px', 
-                textAlign: 'center' 
-            }}>
-            <Avatar 
-                src="/broken-image.jpg" 
-                alt='Gustavo Bodziak' 
-                sx={{ width: '50px', 
-                    height: '50px', 
-                    margin: '10px auto' 
-                }}/>
-            <Typography component='div' variant='h5'>
-                Gustavo Bodziak
-            </Typography>
-            <Typography 
-                gutterbottom 
-                variant="subtitle1" 
-                component="div" 
-                sx={{ color: 'text.secondary' }}
-            >
-                Programador
-            </Typography>
-            <Typography gutterbottom variant='button' component='div'>
-                (41) 98778-0396
-            </Typography>
-            <Typography gutterbottom variant='caption' component='div'>
-                Guuhsilva13@outlook.com
-            </Typography>
-        </CardContent>
-    </>
-)
-
-const UserCard = () => {
+const UserCard = ({ user }) => {
     return (
         <Container sx={{ height: '70vh', alignContent: 'center' }}>
             <Grid container sx={{ justifyContent: 'center' }}>
                 <Grid size={{ xs: 12, sm: 7, md: 4 }}>
-                    <Card variant="outlined">{card}</Card>
+                    <Card variant="outlined">
+                        <CardContent
+                            sx={{
+                                padding: '30px',
+                                textAlign: 'center'
+                            }}>
+                            <Avatar
+                                src="/broken-image.jpg"
+                                alt={user.name.value}
+                                sx={{
+                                    width: '50px',
+                                    height: '50px',
+                                    margin: '10px auto'
+                                }} />
+                            <Typography component='div' variant='h5'>
+                                {user.name.value}
+                            </Typography>
+                            <Typography
+                                gutterbottom
+                                variant="subtitle1"
+                                component="div"
+                                sx={{ color: 'text.secondary' }}
+                            >
+                                 {user.job.value}
+                            </Typography>
+                            <Typography gutterbottom variant='button' component='div'>
+                                {user.phone.value}
+                            </Typography>
+                            <Typography gutterbottom variant='caption' component='div'>
+                                {user.email.value}
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 </Grid>
             </Grid>
         </Container>
